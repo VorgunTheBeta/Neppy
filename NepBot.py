d68e08e9017fd4bc3e23fdd0b1f9d2cf49bf7d16
VorgunTheBeta <kswan007236@gmail.com>
2016-04-04 11:34:42 +1000

    Update NepBot.py
    added plug command

--- NepBot.py
+++ NepBot.py
@@ -51,6 +51,8 @@ def on_message(message):
             yield from bot.send_message(message.channel, "https://www.youtube.com/watch?v=SAp0xO-LwFs")
        	elif message.content.startswith('?reverb'):
        	    yield from bot.send_message(message.channel, "Ravenslofty recommends using this plugin for all your reverb needs~ \nhttp://magnus.smartelectronix.com/#Ambience")
+       	elif message.content.startswith('?plug'):
+       	    yield from bot.send_message(message.channel, "https://valiantghost.com/ ~~ Created by Summonee /nMessage <@127188004216373248> to have your site added~")
 			
 @bot.event
 @asyncio.coroutine
