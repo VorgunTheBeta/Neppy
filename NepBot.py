668779c7ea7fbd2b188e9f75220163b7c5f24937
VorgunTheBeta <kswan007236@gmail.com>
2016-04-05 16:32:08 +1000

    Update NepBot.py

--- NepBot.py
+++ NepBot.py
@@ -55,7 +55,7 @@ def on_message(message):
     	    yield from bot.send_message(message.channel, "https://valiantghost.com/ ~ Created by Summonee \nhttp://www.ldsgamers.com/ ~ Created by mechwd \nMessage <@127188004216373248> to have your site added~")
        	elif message.content.startswith('?shit'):
        	    yield from bot.send_message(message.channel, "Here are some sites that you should learn from what not to do ~ \nhttp://www.lingscars.com/ \nhttps://valiantghost.com/ \nhttp://memekifanwebsite.netlify.com/")
-	elif message.content.startswith('?google'):
+       	elif message.content.startswith('?google'):
 	    google = message.content.replace('?google ', '').replace(" ", '+')
 	    send = 'https://google.com/search?q=' + google
 	    yield from bot.send_message(message.channel, send)
