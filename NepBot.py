2a2d5935e83cddb206d6139cd155ad3c6f5cf9c5
VorgunTheBeta <kswan007236@gmail.com>
2016-04-19 17:51:05 +1000

    Update NepBot.py

--- NepBot.py
+++ NepBot.py
@@ -65,7 +65,7 @@ def on_message(message):
             yield from bot.send_message(message.channel, send)
         elif message.content.startswith('The More You Know') or message.content.startswith('the more you know'):
             yield from bot.send_message(message.channel, "http://cdn.theatlantic.com/assets/media/img/mt/2014/09/The_More_You_Know/lead_large.png")
-        elif 'pudding'in message.content and message.author.id != '167981908918140928':
+        elif 'pudding'.lower() in message.content and message.author.id != '167981908918140928':
             image = random.choice(["http://img09.deviantart.net/336c/i/2013/317/e/3/nep_s_pudding_by_devilnekox-d6u5ae7.jpg", "https://gotgame.com/wp-content/uploads/2015/02/pudding.jpg", "https://53rg10.files.wordpress.com/2013/09/commie_hyperdimension_neptunia_the_animation_-_10_643170f6-mkv_snapshot_19-17_2013-09-14_16-22-45.jpg", "https://media.giphy.com/media/QfUwLcZ1cbaOQ/giphy.gif", "https://i.ytimg.com/vi/2I4Sjqn17QI/hqdefault.jpg", "https://31.media.tumblr.com/f89a86fff4d005aaeb2ff96afe166d86/tumblr_inline_ncxdqpuqSH1rejbig.gif"])
             msg = image + " \nPUDDING~"	
             yield from bot.send_message(message.channel, msg)
