f7f31c6ff1000cdfc374afc1aa1faa8fdec71d32
VorgunTheBeta <kswan007236@gmail.com>
2016-05-06 19:41:43 +1000

    Update NepBot.py

--- NepBot.py
+++ NepBot.py
@@ -105,7 +105,7 @@ def on_message(message):
             f = open(fname, 'a')
             msg = message.content.replace('?mknote ','')
             f.write(msg+"\n")
-            result = dbclient.files_search('/', userid, start=0,max_results=100)
+            result = dbclient.files_search('', userid, start=0,max_results=100)
             dbclient.files_upload(msg, "/"+fname, dropbox.files.WriteMode.update, client_modified=datetime.datetime.now(),mute=True)
             f.close()
             print('Message written')
