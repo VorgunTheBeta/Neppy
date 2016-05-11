c4bee9fc4f84efbe3a74bbed63dde55c40d8152a
VorgunTheBeta <kswan007236@gmail.com>
2016-05-12 09:32:33 +1000

    Update NepBot.py

--- NepBot.py
+++ NepBot.py
@@ -136,7 +136,7 @@ def on_message(message):
             
         elif message.content.startswith('?nickname') and message.author.id =="127188004216373248":
             nickname = message.content.replace('?nickname ','')
-            yield from bot.change_nickname(bot, nickname)
+            yield from bot.change_nickname(nickname)
             prtmsg = "Nickname changed to {0} on {1.name}"
             print(prtmsg.format(nickname, message.server))
 
