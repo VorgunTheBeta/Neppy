846f7a0dabf600761397b23995ece2110a5eb18d
VorgunTheBeta <kswan007236@gmail.com>
2016-05-12 10:01:27 +1000

    Update NepBot.py

--- NepBot.py
+++ NepBot.py
@@ -136,6 +136,8 @@ def on_message(message):
             yield from bot.change_nickname(message.server.me, nickname)
             prtmsg = "Nickname changed to {0} on {1.name}"
             print(prtmsg.format(nickname, message.server))
+        elif bot.mention in message.content:
+            yield from bot.send_message(message.channel, "Yes?~~~")
 
 @bot.event
 @asyncio.coroutine
