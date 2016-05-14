9c764bd2495c36494be366446cc705e6cda0df98
VorgunTheBeta <kswan007236@gmail.com>
2016-05-15 08:46:05 +1000

    Update NepBot.py

--- NepBot.py
+++ NepBot.py
@@ -51,28 +51,32 @@ def on_message(message):
         elif message.content.startswith('?rec txt editor') and message.author.server.id=='154009582748827648':
             yield from bot.send_message(message.channel, "The moderators of this immaculate server highly recommend using Brackets~ \nhttp://brackets.io/")
         elif message.content.startswith('?rec dev site') and message.author.server.id=='154009582748827648':
-       	    yield from bot.send_message(message.channel, "The moderators of this immaculate server highly recommend using Mozilla Developer Network for all your coding help needs~ \nhttps://developer.mozilla.org/en-US/")
+            yield from bot.send_message(message.channel, "The moderators of this immaculate server highly recommend using Mozilla Developer Network for all your coding help needs~ \nhttps://developer.mozilla.org/en-US/")
         elif message.content.startswith('?mods') and message.author.server.id=='154009582748827648':
             yield from bot.send_message(message.channel, "<@127010252934610944>, <@127188004216373248>, <@126899976042184705> you are needed!~")
         elif message.content.startswith('?mods') and message.author.server.id=='156523621240537088':
             yield from bot.send_message(message.channel, "Oracles and leaders of <@&172639681442217984>, you are needed!~")
         elif message.content.startswith('?info') and message.author.server.id=='154009582748827648':
             user = message.server.get_member_named("VorgunTheBeta#9662")
-            msg = "I was created by {0.name}~ \nHere are my commands =```?hello, ?source, ?shit, ?rec browser, ?rec txt editor, ?mods, ?rec dev site, ?reverb, ?plug, ?google [search term], ?imfeelinglucky [search term], ?notice, ?support```"
-            yield from bot.send_message(message.channel, msg.format(user))
+            if user.nick == None:
+               name = user.name
+            else:
+               name = user.nick
+            msg = "I was created by {0}~ \nHere are my commands =```?hello, ?source, ?shit, ?rec browser, ?rec txt editor, ?mods, ?rec dev site, ?reverb, ?plug, ?google [search term], ?imfeelinglucky [search term], ?notice, ?support```"
+            yield from bot.send_message(message.channel, msg.format(name))
         elif message.content.startswith('?help') and message.author.server.id!='154009582748827648':
             yield from bot.send_message(message.channel, "I was created by <@127188004216373248>~ \nHere are my commands =```?hello, ?source, ?google [search term], ?imfeelinglucky [search term], The More You Know, ?pudding, ?help, ?smug, ?shock, ?wat, ?notice, ?support```")
         elif message.content.startswith('?lol') and message.author.server.id=='154009582748827648':
             yield from bot.send_message(message.channel, "http://ta-sa.org/data/images/laughing_man_big_2.png")
         elif message.content.startswith('limewire') and message.author.server.id=='154009582748827648':
             yield from bot.send_message(message.channel, "https://www.youtube.com/watch?v=SAp0xO-LwFs")
-       	elif message.content.startswith('?reverb') and message.author.server.id=='154009582748827648':
-    	    yield from bot.send_message(message.channel, "Ravenslofty recommends using this plugin for all your reverb needs~ \nhttp://magnus.smartelectronix.com/#Ambience")
-       	elif message.content.startswith('?plug') and message.author.server.id=='154009582748827648':
-    	    yield from bot.send_message(message.channel, "https://github.com/VorgunTheBeta/Neppy ~ Hey thats me, Created by VorgunTheBeta \nhttps://valiantghost.com/ ~ Created by Summonee \nhttp://www.ldsgamers.com/ ~ Created by mechwd \nhttp://www.cibgraphics.com ~ Created by mechwd \nhttps://github.com/cibgraphics/Vanilla5 ~ Created by mechwd \nhttp://www.cyvercom.com/ ~ Created by orctamer \nhttp://xandsoftware.azurewebsites.net/ ~Created by XAND(Eric) \nMessage <@127188004216373248> to have your site added~")
-       	elif message.content.startswith('?shit') and message.author.server.id=='154009582748827648':
-       	    yield from bot.send_message(message.channel, "Here are some sites that you should learn from what not to do ~ \nhttp://www.lingscars.com/ \nhttp://www.fentek-ind.com/ \nhttps://valiantghost.com/ \nhttp://memekifanwebsite.netlify.com/")
-       	elif message.content.startswith('?google'):
+        elif message.content.startswith('?reverb') and message.author.server.id=='154009582748827648':
+            yield from bot.send_message(message.channel, "Ravenslofty recommends using this plugin for all your reverb needs~ \nhttp://magnus.smartelectronix.com/#Ambience")
+        elif message.content.startswith('?plug') and message.author.server.id=='154009582748827648':
+            yield from bot.send_message(message.channel, "https://github.com/VorgunTheBeta/Neppy ~ Hey thats me, Created by VorgunTheBeta \nhttps://valiantghost.com/ ~ Created by Summonee \nhttp://www.ldsgamers.com/ ~ Created by mechwd \nhttp://www.cibgraphics.com ~ Created by mechwd \nhttps://github.com/cibgraphics/Vanilla5 ~ Created by mechwd \nhttp://www.cyvercom.com/ ~ Created by orctamer \nhttp://xandsoftware.azurewebsites.net/ ~Created by XAND(Eric) \nMessage <@127188004216373248> to have your site added~")
+        elif message.content.startswith('?shit') and message.author.server.id=='154009582748827648':
+            yield from bot.send_message(message.channel, "Here are some sites that you should learn from what not to do ~ \nhttp://www.lingscars.com/ \nhttp://www.fentek-ind.com/ \nhttps://valiantghost.com/ \nhttp://memekifanwebsite.netlify.com/")
+        elif message.content.startswith('?google') or message.content.startswith('?Google'):
             google = message.content.replace('?google ', '').replace(" ", '+').replace("<", '%3C').replace(">", '%3E')
             send = 'https://google.com/search?q=' + google
             yield from bot.send_message(message.channel, send)
@@ -84,7 +88,7 @@ def on_message(message):
             yield from bot.send_message(message.channel, "http://cdn.theatlantic.com/assets/media/img/mt/2014/09/The_More_You_Know/lead_large.png")
         elif message.content.startswith('?pudding'):
             image = random.choice(["http://img09.deviantart.net/336c/i/2013/317/e/3/nep_s_pudding_by_devilnekox-d6u5ae7.jpg", "https://gotgame.com/wp-content/uploads/2015/02/pudding.jpg", "https://53rg10.files.wordpress.com/2013/09/commie_hyperdimension_neptunia_the_animation_-_10_643170f6-mkv_snapshot_19-17_2013-09-14_16-22-45.jpg", "https://media.giphy.com/media/QfUwLcZ1cbaOQ/giphy.gif", "https://i.ytimg.com/vi/2I4Sjqn17QI/hqdefault.jpg", "https://31.media.tumblr.com/f89a86fff4d005aaeb2ff96afe166d86/tumblr_inline_ncxdqpuqSH1rejbig.gif","https://images-2.discordapp.net/.eJwNwsENwyAMAMBdGACDAQPZBhFEUiUFYfcVdff2dI_6rEtt6hCZvAHsJ9exds0yVulN9zH61co8WddxQxEp9bjbWxis85QwWRPJ5OBSILAxkY8phkBko0eTwXrCfwzZIerX7Or7A8AIIn8.jbMjCJpIS-MJrTyegz9OrU1LPuQ.jpg"])
-            msg = image + " \nPUDDING~"	
+            msg = image + " \nPUDDING~" 
             yield from bot.send_message(message.channel, msg)
         elif message.content.startswith('?smug'):
             img = random.choice(["http://scontent-a.cdninstagram.com/hphotos-xaf1/t51.2885-15/10914676_1598608667029772_1771256047_a.jpg","http://41.media.tumblr.com/eae2e1da5f744b4058274b7f3ce93463/tumblr_nn129eZTXb1urtbs9o1_1280.png","https://i.warosu.org/data/jp/img/0140/65/1441685647187.jpg","https://cdn.discordapp.com/attachments/156523621240537088/177982860093816833/1435607123227.jpg"])
@@ -156,10 +160,10 @@ def on_message(message):
 @bot.event
 @asyncio.coroutine
 def on_member_join(member):
-	if member.server.id=='154009582748827648':
-		channel = discord.Object(id='155225555598442496')
-		message = "{0.name} has joined the server~ <@127010252934610944>"
-		yield from bot.send_message(channel, message.format(member))
+        if member.server.id=='154009582748827648':
+                channel = discord.Object(id='155225555598442496')
+                message = "{0.name} has joined the server~ <@127010252934610944>"
+                yield from bot.send_message(channel, message.format(member))
 
 @bot.event
 @asyncio.coroutine
@@ -170,7 +174,7 @@ def on_ready():
     print('-----')
     print("Connected Servers:")
     for s in bot.servers:
-    	print(s.name)
+        print(s.name)
 
 
 
