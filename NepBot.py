4b11ba6dd4c10502cd52017124cd90a1f54bde2e
VorgunTheBeta <kswan007236@gmail.com>
2016-06-14 18:52:35 +1000

    modified notice command to use a txt file

--- NepBot.py
+++ NepBot.py
@@ -115,8 +115,11 @@ def on_message(message):
         elif message.content.startswith('?wat'):
             yield from bot.send_message(message.channel, "https://cdn.discordapp.com/attachments/156523621240537088/180092100199579650/1457714637653.gif WAT")
         elif message.content.startswith('?notice'):
-            msg = "14/6: Added a new command, ?image. It doesnt embed, but it works. Im working on getting it to embed~"
+            fname = "notice.txt"
+            f = open(fname)
+            msg = f.read()
             yield from bot.send_message(message.channel, msg)
+            f.close()
         elif message.content.startswith('?mknote'):
             yield from bot.send_message(message.channel, "Please do not use this command, it does not work properly~~~")
             #userid= message.author.id
@@ -143,7 +146,7 @@ def on_message(message):
              #   yield from bot.send_message(message.channel, "I'm so sorry, but I can't seem to find any notes for you~~~")    
             #else:
              #   dbclient.files_download_to_file('/'+fname,'/'+fname)
-              #  g = open(fname)
+                #  g = open(fname)
                # mes = g.read()
                # yield from bot.send_message(message.channel, "Your saved notes are: "+mes)
             
