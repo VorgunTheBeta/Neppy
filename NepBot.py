217a5fb750beb8f5b5ca70e82c693905c7646178
VorgunTheBeta <kswan007236@gmail.com>
2016-06-17 09:34:09 +1000

    changed some things

--- NepBot.py
+++ NepBot.py
@@ -263,6 +263,8 @@ def on_message(message):
                 yield from bot.send_message(user, pm.format(requestee))
         elif message.content.lower() == "?crying nepgear":
             yield from bot.send_message(message.channel, "http://i.imgur.com/mvxtHrr.png")
+        elif message.content.lower() == "?nepune"
+            yield from bot.send_message(message.channel, random.choice(FileToArray("Text Stuff/neptune.txt")))
 
 
 @bot.event
