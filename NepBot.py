a56a1cbc64310ad4b343d0b271cc2d5e4aaa12d6
VorgunTheBeta <kswan007236@gmail.com>
2016-06-17 20:16:37 +1000

    i can english very great

--- NepBot.py
+++ NepBot.py
@@ -285,7 +285,7 @@ def on_message(message):
         elif message.content == "AAA":
             yield from bot.send_message(message.channel, "MOU")
         elif message.content == "?changepic":
-            if message.auuthor.id == '127188004216373248':
+            if message.author.id == '127188004216373248':
                 image = open("profilepic.png")
                 yield from bot.edit_profile(avatar=image.read())
                 image.close()
