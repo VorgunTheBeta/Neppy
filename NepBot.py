f18463d452f983e1361485ba28bafc38c784d048
VorgunTheBeta <kswan007236@gmail.com>
2016-06-17 11:59:24 +1000

    fucking lambda

--- NepBot.py
+++ NepBot.py
@@ -279,7 +279,7 @@ def on_message(message):
             else:
                 user = discord.User()
                 hugee = message.content.replace("?hug ",'')
-                user = utils.get(message.server.members, name = hugee)
+                user = utils.find(lambda m: m.name == hugee, message.server.members)
                 msg = "*hugs {0.mention}*"
                 yield from bot.send_message(message.channel, msg.format(user))
 
