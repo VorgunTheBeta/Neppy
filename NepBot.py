961136906ad61309edf160b81be83d9092a5c8aa
VorgunTheBeta <kswan007236@gmail.com>
2016-06-26 17:01:10 +1000

    this will be better

--- NepBot.py
+++ NepBot.py
@@ -243,7 +243,7 @@ async def on_message(message):
             await bot.send_typing(message.channel)
             await asyncio.sleep(2)
             await bot.send_message(message.channel, msg.format(message.author))
-        elif message.content.lower() == "fucking heroku":
+        elif message.content.startswith("fucking ").lower():
             await bot.send_typing(message.channel)
             await asyncio.sleep(2)
             await bot.send_message(message.channel, "It sucks, I know.")
