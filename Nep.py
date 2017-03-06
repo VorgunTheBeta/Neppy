a0621efe98e39ef443f686f9707768ab9f128d18
VorgunTheBeta <kswan007236@gmail.com>
2017-03-06 05:02:07 +0000

    Made some needed changes

--- Nep.py
+++ Nep.py
@@ -46,7 +46,7 @@ def blancship():
         return ctx.message.server.id =='199045411640573954'
     return commands.check(predicate)
 
-@bot.command()
+@bot.command(help="Says hello")
 async def hello():
     result = "Hello there~"
     await bot.type()
@@ -72,7 +72,7 @@ async def sleep(ctx, member: discord.Member = None):
         await asyncio.sleep(2)
         await bot.say("http://www.ozsticker.com/ozebayimages/620_dave_product.jpg")
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,hidden=True)
 @owner()
 async def status(ctx):
         msg = ctx.message.content
@@ -159,25 +159,26 @@ async def nick(nickname :str):
     prtmsg = "Nickname changed to {0} on {1.name}"
     print(prtmsg.format(nickname, message.server))
 
-@bot.command()
+@bot.command(hidden=True)
+@owner
 async def join_server():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( "So you want me in your server?~~~ Just use this link: https://goo.gl/NPrZRF")
 
-@bot.command(aliases=['sauce'])
+@bot.command(aliases=['sauce'],help="Shows my source code")
 async def source():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( "So you want to see whats behind me huh~ https://github.com/VorgunTheBeta/Neppy")
 
-@bot.command()
+@bot.command(help="Shows any relevant notices")
 async def notice():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( Text("notice.txt"))
 
-@bot.command(pass_context=True,aliases=['Google'])
+@bot.command(pass_context=True,aliases=['Google'],help="Google things")
 async def google(ctx):
     src = ctx.message.content.replace("?google ",'').replace(" ", '+').replace("<", '%3C').replace(">", '%3E')
     send = 'https://google.com/search?q=' + src
@@ -185,7 +186,7 @@ async def google(ctx):
     await asyncio.sleep(2)
     await bot.say(send)
 
-@bot.command()
+@bot.command(help="Do you feel lucky?")
 async def im_feeling_lucky(search: str):
     src = search.replace(" ", '').replace("<", '%3C').replace(">", '%3E')
     send = 'https://google.com/search?btnI=&q=' + src
@@ -193,7 +194,7 @@ async def im_feeling_lucky(search: str):
     await asyncio.sleep(2)
     await bot.say(send)
 
-@bot.command()
+@bot.command(help="Google image search things")
 async def image(search: str):
     img = search.replace(" ", '+')
     send= "https://google.com/search?tbm=isch&q=" + img
@@ -202,7 +203,7 @@ async def image(search: str):
     await bot.say( send)
     
     
-@bot.command()
+@bot.command(help="Look things up on Wikipedia")
 async def wiki(search: str):
     src = search.replace(" ",'_')
     send = "https://en.wikipedia.org/wiki/"+src
@@ -210,7 +211,7 @@ async def wiki(search: str):
     await asyncio.sleep(2)
     await bot.say(send)
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,help="Make notes that will be saved")
 async def make_note(ctx):
     userid= ctx.message.author.id
     note = ctx.message.content.replace("?make_note ",'')
@@ -230,7 +231,7 @@ async def make_note(ctx):
     g.close()
     print('Message written')
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,help="View any notes that have been saved")
 async def notes(ctx):
      userid= ctx.message.author.id
      fname = userid + ".txt"
@@ -244,7 +245,7 @@ async def notes(ctx):
         mes = g.read()
         await bot.say( "Your saved notes are: "+mes)
 
-@bot.command()
+@bot.command(help="If you want to help out Vorgun")
 async def support():
     await bot.type()
     await asyncio.sleep(2)
@@ -253,7 +254,8 @@ async def support():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say("Or if you prefer to make a single donation, go here: https://paypal.me/vorgunthebeta")
-@bot.command()
+    
+@bot.command(help="Show current supporters of the patreon")
 async def supporters():
     highrank = "Noire.io"
     lowrank = "Games Von Dudemeister, blan"
@@ -285,43 +287,43 @@ async def sagashite():
     await asyncio.sleep(2)
     await bot.say("!p https://www.youtube.com/watch?v=I7TUNfNGTzY")
 
-@bot.command(aliases=['Blanc','White_Heart'])
+@bot.command(aliases=['Blanc','White_Heart'], help="Shows pictures of Blanc/White Heart")
 async def blanc():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("blanc.txt"))
 
-@bot.command(aliases=['Nepgear','Purple_Sister'])
+@bot.command(aliases=['Nepgear','Purple_Sister'], help="Shows pictures of Nepgear/Purple Sister")
 async def nepgear():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("nepgear.txt"))
 
-@bot.command(aliases=['ploot','Plutia','Iris_Heart'])
+@bot.command(aliases=['ploot','Plutia','Iris_Heart'], help="Shows pictures of Plutia/Iris Heart")
 async def plutia():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("plutia.txt"))
 
-@bot.command(aliases=['Noire', 'Black_Heart'])
+@bot.command(aliases=['Noire', 'Black_Heart'], help="Shows pictures of Noire/Black Heart")
 async def noire():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("noire.txt"))
 
-@bot.command(aliases=['Vert','Green_Heart'])
+@bot.command(aliases=['Vert','Green_Heart'],, help="Shows pictures of Vert/Green Heart")
 async def vert():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("vert.txt"))
 
-@bot.command(aliases=['ram_and_rom','Rom_and_Ram', 'Ram_and_Rom', 'White_Sisters'])
+@bot.command(aliases=['ram_and_rom','Rom_and_Ram', 'Ram_and_Rom', 'White_Sisters'], help="Shows pictures of Rom and Ram/White Sisters")
 async def rom_and_ram():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("romandram.txt"))
 
-@bot.command(aliases=['Uni','Black_Sister'])
+@bot.command(aliases=['Uni','Black_Sister'], help="Shows pictures of Uni/Black Sister")
 async def uni():
     await bot.type()
     await asyncio.sleep(2)
@@ -333,7 +335,7 @@ async def nepger():
     await asyncio.sleep(2)
     await bot.say( "http://i.imgur.com/XHkYq9d.png")
 
-@bot.command(aliases=['histy','Histoire','Histy'])
+@bot.command(aliases=['histy','Histoire','Histy'], help="Shows pictures of Histoire")
 async def histoire():
     await bot.type()
     await asyncio.sleep(2)
@@ -345,31 +347,31 @@ async def crying_nepgear():
     await asyncio.sleep(2)
     await bot.say( "http://i.imgur.com/mvxtHrr.png")
 
-@bot.command(aliases=['iffy','Iffy'])
+@bot.command(aliases=['iffy','Iffy'], help="Shows pictures of IF")
 async def IF():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("IF.txt"))
 
-@bot.command(aliases=['Compa'])
+@bot.command(aliases=['Compa'], help="Shows pictures of Compa")
 async def compa():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("compa.txt"))
 
-@bot.command(aliases=['Neptune','Purple_Heart', 'nep'])
+@bot.command(aliases=['Neptune','Purple_Heart', 'nep'], help="Shows pictures of Neptune/Purple Heart")
 async def neptune():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("neptune.txt"))
 
-@bot.command(aliases=['Marvy'])
+@bot.command(aliases=['Marvy'], help="Shows pictures of MarvelousAQL")
 async def marvy():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("marvy.txt"))
 
-@bot.command()
+@bot.command(help="Sends a wallpaper")
 async def wallpaper():
     await bot.type()
     await asyncio.sleep(2)
@@ -389,13 +391,13 @@ async def hug(ctx, name: str):
 async def change_pic(image: str):
     await ChangePic(image)
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,help="For requesting the bot in your server, or a feature/command")
 async def request(ctx,req: str):
     requestee = ctx.message.author
     server = ctx.message.server
     await MakeNepRequest(req, requestee, server)
 
-@bot.command()
+@bot.command(help="Pray to the RNG gods")
 async def RNG():
     await bot.type()
     await asyncio.sleep(2)
@@ -409,40 +411,40 @@ async def lol():
     await asyncio.sleep(2)
     await bot.say( "http://ta-sa.org/data/images/laughing_man_big_2.png")
 
-@bot.command()
+@bot.command(help="Posts an old programmer joke")
 async def limewire():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( "https://www.youtube.com/watch?v=SAp0xO-LwFs")
 
-@bot.command()
+@bot.command(help="PUDDING")
 async def pudding():
     msg = RandomImage("pudding.txt") + " \nPUDDING~"
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( msg)
 
-@bot.command()
+@bot.command(help="When in need of a smug face")
 async def smug():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say( RandomImage("smug.txt"))
 
-@bot.command()
+@bot.command(help="When you need a shock gif")
 async def shock():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say(
                            "https://cdn.discordapp.com/attachments/156523621240537088/176951338356178945/jeepers.gif")
 
-@bot.command()
+@bot.command(help="For use when utterly confused")
 async def wat():
     await bot.type()
     await asyncio.sleep(2)
     await bot.say(
                            "https://cdn.discordapp.com/attachments/156523621240537088/180092100199579650/1457714637653.gif WAT")
 
-@bot.command()
+@bot.command(help="To know the story behind me")
 async def story():
     await bot.type()
     await asyncio.sleep(2)
@@ -509,7 +511,7 @@ async def on_message(message):
         
 
         if message.content.startswith("?"):
-            if " " in message.content and "?rec" not in message.content and "make_note" not in message.content and "google" not in message.content and "image" not in message.content and "status" not in message.content and "nick" not in message.content and "request" not in message.content and "change" not in message.content and "hug" not in message.content:
+            if " " in message.content and "?rec" not in message.content and "make_note" not in message.content and "google" not in message.content and "image" not in message.content and "status" not in message.content and "nick" not in message.content and "request" not in message.content and "change" not in message.content and "hug" not in message.content and "im_feeling_lucky" not in message.content and "wiki" not in message.content:
                 message.content = message.content.replace(" ","_")
 # end of text recog commands
         await bot.process_commands(message)
@@ -535,6 +537,7 @@ async def on_ready():
     print("Connected Servers:")
     for s in bot.servers:
         print(s.name)
+    print(count_iterable(bot.servers))
     user = discord.User()
     user.id = 127188004216373248
     await bot.send_message(user, "I'm up")
@@ -570,6 +573,9 @@ async def MakeNepRequest(msg,requestee,source):
     user = discord.User()
     user.id = 127188004216373248
     await bot.send_message(user, pm.format(requestee, source.name))
+
+def count_iterable(i):
+    return sum(1 for e in i)
 
 #end of custom functions
 bot.run(Text('token.txt'))
